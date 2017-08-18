81a8d57b10b23f5f553d5340832985af060a7e23
test <chris@intelatek.com>
2017-08-18 02:21:33 +0000

    Admin functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,6 +37,13 @@ class UsersController < ApplicationController
         @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
     end
     
+    def destroy
+        @user = User.find(params[:id])
+        @user.destroy
+        flash[:danger] = "User and all articles created by user have been deleted"
+        redirect_to users_path
+    end
+    
     private
     
     def user_params
@@ -48,7 +55,7 @@ class UsersController < ApplicationController
     end
 
     def require_same_user
-      if current_user != @user
+      if current_user != @user and !current_user.admin?
         flash[:danger] = "You can only edit your own profile."
         redirect_to root_path
       end
