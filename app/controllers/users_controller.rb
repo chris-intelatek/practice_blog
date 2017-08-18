a0912f628ddd3b48f613f60169f5836967644e46
test <chris@intelatek.com>
2017-08-18 03:11:07 +0000

    Fix destroy articles when destroying user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
     before_action :set_user, only: [:edit, :update, :show]
     before_action :require_same_user, only: [:edit, :update]
-  
+    before_action :require_admin, only: [:destroy]
+    
     def index
         @users = User.paginate(page: params[:page], per_page: 5)
     end
@@ -59,6 +60,13 @@ class UsersController < ApplicationController
         flash[:danger] = "You can only edit your own profile."
         redirect_to root_path
       end
+    end
+    
+    def require_admin
+        if logged_in? and !current_user.admin?
+            flash[:danger] = "Only admin users can perform that action"
+            redirect_to root_path
+        end
     end
     
 end
