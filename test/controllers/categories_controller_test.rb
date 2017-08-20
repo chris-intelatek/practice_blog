e0722b9c8832522c83c608bca20db5750ddc37e0
test <chris@intelatek.com>
2017-08-20 16:38:24 +0000

    Restrict creation of categories to Admin and test

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -4,15 +4,16 @@ class CategoriesControllerTest < ActionController::TestCase
   
   def setup
     @category = Category.create(name:"sports")
+    @user = User.create(username: "johntest", email: "john@example.com", password: "password", admin: true)
   end
   
-
   test "should get categories index" do
     get :index
     assert_response :success
   end
   
   test "should get categories new" do
+    session[:user_id] = @user.id
     get :new
     assert_response :success    
   end
@@ -22,4 +23,10 @@ class CategoriesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "should redirect create when admin not logged in" do
+    assert_no_difference 'Category.count' do
+      post :create, category: { name: "sports" }
+    end
+    assert_redirected_to categories_path
+  end
 end
